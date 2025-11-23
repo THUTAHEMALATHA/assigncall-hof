4e41473de33ff4bbb699b6750bb590bd7fa9a1a5
THUTAHEMALATHA <thutahemalatha@gmail.com>
2025-11-23 18:26:46 +0530

    Understanding Global and Function Execution Context

---
+++ colsures/Question19.js
@@ -0,0 +1,11 @@
+let age=25;
+function displayAge(){
+    console.log("currentage": age);
+}
+function changeAge(){
+    age = newAge
+    console.log("changeage":age);
+}
+displayAge();
+changeAge(30);
+displayAge();
